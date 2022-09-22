b460c86262022a2fa3e4c2d14709e7e92d0f7168
Nguyen Huy Hieu <hieunhgch18038@gmail.com>
2022-09-22 17:19:54 +0700

    process.env.PORT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const port = 9000;
+const port = process.env.PORT || 9000;
 const app = http.createServer((req, res) => {
     res.write("<h1>Hello world</h1>");
     res.write("<h3>My node js page</h3>");
